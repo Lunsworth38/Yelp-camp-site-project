abffdd483266bae118556a8ada3333b1cac7dab1
Lewis Unsworth <lunsworth@live.com>
2018-03-05 14:45:06 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://Lewis:Fandango38@ds163701.mlab.com:63701/yelpcamp");
+// mongodb://Lewis:Fandango38@ds163701.mlab.com:63701/yelpcamp
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
